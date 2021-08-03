890dcfa14a0dc379bc6486b98449aab1d92afc8c
Sumayyah Taiwo <sumty4deen@gmail.com>
2021-08-03 11:26:19 +0100

    Added file containing password_generator function

---
+++ paasword_generator.py
@@ -0,0 +1,11 @@
+def password_generator(passwordLength):
+    """
+    This function takes in one argument, the length of password to be generated
+    and returns a generated alphanumeric password as a string
+
+    INPUT - passwordLength(int) - length of password to be generated
+
+    OUTPUT - password(str) - generated alphanumeric password
+
+    """
+    
