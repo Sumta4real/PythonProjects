f737a234da27a6e378c4416a3789dfdd37bbaf38
Sumayyah Taiwo <sumty4deen@gmail.com>
2021-09-11 19:22:02 +0100

    added unit test for passord generator

--- test_file.py
+++ test_file.py
@@ -10,3 +10,14 @@ def test_scrabbleScore():
     assert word_score('jezebel') == 25
     assert word_score('quizzify') == 41
     assert word_score('fortune') == 10
+
+
+#create unit test for the password_generator function
+from paasword_generator import password_generator
+@pytest.mark.password_generator
+def test_passwordgenerator():
+    assert len(password_generator(8)) == 8 , "password length not equal to the specified length"
+    assert password_generator(6)
+    
+    
+from paasword_generator import password_generator
