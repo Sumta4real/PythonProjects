7680fb89adfd17d1b63280bce15ec544436b6b4c
Taiwo Sumayyah <31887313+Sumta4real@users.noreply.github.com>
2022-05-19 03:42:02 -0700

    Moved control keys codes into the while loop

--- snake_game/main.py
+++ snake_game/main.py
@@ -27,15 +27,16 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-screen.listen()
-screen.onkey(snake.up,'Up')
-screen.onkey(snake.down,'Down')
-screen.onkey(snake.left,'Left')
-screen.onkey(snake.right,'Right')
-
 
 game_is_on = True
 while game_is_on:
+    
+    screen.listen()
+    screen.onkey(snake.up,'Up')
+    screen.onkey(snake.down,'Down')
+    screen.onkey(snake.left,'Left')
+    screen.onkey(snake.right,'Right')
+
     screen.update()
     time.sleep(0.1)
     snake.move()
